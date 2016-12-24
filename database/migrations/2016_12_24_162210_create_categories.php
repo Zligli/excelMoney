7dc93e67bca8c16f25e8a59b2aef93eea7a1132b
Glisovic <glisovicnikola@gmail.com>
2016-12-24 17:46:47 +0100

    Migrations made

---
+++ database/migrations/2016_12_24_162210_create_categories.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('main_category_id')->unsigned();
+            $table->foreign('main_category_id')->references('id')->on('main_categories')->onDelete('cascade');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
